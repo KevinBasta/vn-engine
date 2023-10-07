d01e511f940c9250b9538ad68f9bc77545cc1d38
Kevin Basta <kevinbastadeveloper@gmail.com>
2023-10-07 19:56:35 -0400

    Initial cmake setup

---
+++ visual novel engine/vn_engine.cpp
@@ -0,0 +1,12 @@
+﻿// visual novel engine.cpp : Defines the entry point for the application.
+//
+
+#include "vn_engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
